4bfd2451d1cfdbfe760a687cfbd3bdef36136a22
izen.oku <izenhuang@hotmail.com>
2020-09-22 20:58:22 -0700

    feat: leaderRouter.js updated w mongoose model functions

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -16,15 +16,29 @@ leaderRouter.route('/')
 	next(); // will continue to look for functions that match '/dishes/ endpoint
 })
 .get((req, res, next) => {
-
+	Leader.find({})
+	.then((leaders) => {
+		res.json(leaders);
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 .post((req, res, next) => {
+	Leader.create(req.body)
+	.then((leader) => {
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 .put((req, res, next) => {
 	res.statusCode = 403;
 	res.end('PUT operation not supported on /leader');
 })
 .delete((req, res, next) => {
+	Leader.remove({})
+	.then((leader) => {
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 
 leaderRouter.route('/:leaderId')
@@ -35,17 +49,37 @@ leaderRouter.route('/:leaderId')
 })
 .get((req, res, next) => {
 	// GET result for a specified leader
+	const { params } = req;
+
+	Leader.findById(params.leaderId)
+	.then((leader) => {
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 .post((req, res, next) => {
 	// POST operation not supported on specified promo
 	res.statusCode = 403;
-	res.end(`POST operation not supported on /dishes/${req.params.dishId}`);r
+	res.setHeader('Content-Type', 'html/txt');
+	res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
 })
 .put((req, res, next) => {
 	// UPDATE specified leader
+	Leader.findByIdAndUpdate(req.params.leaderId, {
+		$set: req.body
+	}, { new: true })
+	.then((leader) => {
+		res.json(leader)
+	}, (err) => next(err))
+	.catch((err) => next(err));
 })
 .delete((req, res, next) => {
 	// DELETE a specified leader
-})
+	Leader.findByIdAndRemove(req.params.leaderId)
+	.then((leader) => {
+		res.json(leader);
+	}, (err) => next(err))
+	.catch((err) => next(err));
+});
 
 module.exports = leaderRouter;
