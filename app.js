625d64f4aad54c6535a3c3d236cfec9bc1b334f5
izen.oku <izenhuang@hotmail.com>
2020-09-23 17:58:12 -0700

    feat: basic auth

--- app.js
+++ app.js
@@ -20,6 +20,48 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  // we will change request headers to include auth
+
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if (!authHeader) // if client did not provide auth header
+  {
+    var err = new Error('You are not authenticated.')
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401; // unauthorized access
+    next(err);
+    return;
+  }
+  // else we know auth header exists: 
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); 
+  // because auth header is "Basic QWxh..."
+  // then split again because username & password separated by ':'
+
+  var username = auth[0];
+  var password = auth[1];
+
+  // for the moment, use mock username & password
+  if (username === 'admin' && password === 'password') {
+    next();
+  }
+  else {
+    // generate error, challenge client to send in correct authorization
+    var err = new Error('You are not authenticated.');
+
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+
+// authentification middleware
+// clients should have to go through authentification middleware to use routes
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
