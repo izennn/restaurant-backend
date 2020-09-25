75531fe607d43ed793d1f1ea9a1efe6f3cd1da5b
izen.oku <izenhuang@hotmail.com>
2020-09-24 21:57:54 -0700

    fix: moved app.use '/' and 'users' before auth middleware

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/usersRouter');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -31,74 +31,51 @@ app.use(session({
   store: new FileStore()
 }));
 
-// we provide a secret key to encrypt & sign the cookie sent from server to client
-// app.use(cookieParser('12345-67890-09876-54321')); 
+// un-authenticated users can still access homepage and login paths
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-function notAuthenticatedErrResponse(res) {
-  var err = new Error('You are not authenticated');
-  res.setHeader('WWW-Authenticate', 'Basic');
-  err.status = 403;
+/* we provide a secret key to encrypt & sign the cookie sent from server to client
+app.use(cookieParser('12345-67890-09876-54321')); 
+NOTE: we will not need cookieParser once we've installed express-session middleware */
+
+/* return new Error with custom message & status */
+function createErr(message, statusCode) {
+  var err = new Error(message);
+  err.status = statusCode;
   return err;
 }
 
+/* auth middleware to be ran before dish, leader, promotion routers */
 function auth(req, res, next) {
   // check if client req has properties signedCookies or signedCookies.user: 
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    // if client req does not contain auth header: 
-    if (!authHeader) 
-    {
-      next(notAuthenticatedErrResponse(res));
-      return;
-    }
-    // else we know auth header exists: 
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); 
-    var username = auth[0];
-    var password = auth[1];
-
-    // for the moment, use mock username & password
-    if (username === 'admin' && password === 'password') {
-      // include 'user' into the singed cookie with value 'admin'
-      // now all subsequent calls to client will include this cookie
-
-      // res.cookie('user', 'admin', { signed: true }); 
-      req.session.user = 'admin';
-      next();
-    }
-    else {
-      // generate error, challenge client to send in correct authorization
-      next(notAuthenticatedErrResponse(res));
-    }    
+    return next(createErr('You are not authenticated!', 403));
   }
-  else { // if req contains signed cookie 'user'
-    if (req.session.user === 'admin') {
+  else { 
+    // if req contains session information
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
-      return next(notAuthenticatedErrResponse(res));
+      return next(createErr('You are not authenticated!', 403));
     }
   }
 }
-
-// authentification middleware
-// this middleware is put before: express.static and routes
 app.use(auth);
 
+/* express static */
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* custom routes that need to be put AFTER authentication */
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // mongoose server
 const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
-
 // establish connection to Mongo server
 const url = 'mongodb://localhost:27017/restaurant';
 const connect = mongoose.connect(
