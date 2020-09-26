79dd48a5e4a2446b6878d875fb8ce600ec51481b
izen.oku <izenhuang@hotmail.com>
2020-09-25 21:18:45 -0700

    feat: on signin, fsave firstname & lastname from req.body before authenticating passport

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -34,13 +34,25 @@ router.post('/signup', (req, res, next) => {
     }
     else {
       // authenticate again with passport
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-          success: true, // quickly check if registration successful
-          status: 'Registration Successful!',
-          user: user
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCod = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({
+            success: true, // quickly check if registration successful
+            status: 'Registration Successful!',
+            user: user
+          });
         });
       })
     }
