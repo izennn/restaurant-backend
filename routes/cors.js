4e7969140e202e6330c32e744404a4750040f01d
izen.oku <izenhuang@hotmail.com>
2020-09-29 20:23:25 -0700

    feat: cors.cors cors.corsWithOptions methods

---
+++ routes/cors.js
@@ -0,0 +1,30 @@
+/*
+ * Put all CORS realted methods here
+ */
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// all the origins the server is willing to accept
+const whitelist = [
+	'http://localhost:3000', 
+	'http://localhost:3443'
+]
+
+let corsOptionsDelegate = (req, callback) => {
+	let corsOptions;
+
+	if (whitelist.indexOf(req.header('Origin') === -1)) {
+		corsOptions = { origin: true }
+	}
+	else {
+		corsOptions = { origin: false }
+	}
+	callback(null, corsOptions);
+};
+
+// reply access-control-allow-origin: *
+exports.cors = cors();
+// reply access-control-allow-origin: with options
+exports.corsWithOptions = cors(corsOptionsDelegate);
