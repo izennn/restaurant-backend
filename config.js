9d3aec2b363ef7936841b903d881573111ef1fe4
izen.oku <izenhuang@hotmail.com>
2020-10-01 16:46:15 -0700

    feat: facebook clientID & clientSecret

--- config.js
+++ config.js
@@ -2,5 +2,9 @@
 
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/restaurant'
+	'mongoUrl': 'mongodb://localhost:27017/restaurant',
+	'facebook': {
+		clientId: '749507132277190',
+		clientSecret: 'c33f8a54ff041d409b87b141c0092ebd'
+	}
 }
